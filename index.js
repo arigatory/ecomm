f3ba02616c07636a00787f141a897b853b6e55aa
Ivan MSI <arigatory@gmail.com>
2022-04-10 03:42:32 +0300

    my bad

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const usersRepo = require('./repositories/users');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({
+    keys: ['alskfjlksdj']
+}));
 
 app.get('/', (req, res) => {
     res.send(`
         <div>
+            Your id is: ${req.session.userId}
             <form method="POST">
                 <input name="email" placeholder="email" />
                 <input name="password" placeholder="password" />
@@ -19,8 +25,25 @@ app.get('/', (req, res) => {
 });
 
 
-app.post('/', (req, res) => {
-    console.log(req.body);
+app.post('/', async (req, res) => {
+    const { email, password, passwordConfirmation} = req.body;
+
+    const existingUser = await usersRepo.getOneBy({ email });
+
+    if (existingUser) {
+        return res.send('Email in use');
+    }
+
+    if (password != passwordConfirmation) {
+        return res.send('Password must match');
+    }
+
+    // created
+    const user = await usersRepo.create({ email, password });
+
+    // store the id inside cookie
+    req.session.userId = user.Id;
+
     res.send(`
         <div>Account created!</div>
     `);
