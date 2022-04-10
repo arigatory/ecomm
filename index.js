732601b51ae1e9d8a14581d8b333b83d28c1c33d
Ivan MSI <arigatory@gmail.com>
2022-04-10 21:18:35 +0300

    start validation

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
-const usersRepo = require('./repositories/users');
+const authRouter = require('./routes/admin/auth');
+
 
 const app = express();
 
@@ -9,45 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({
     keys: ['alskfjlksdj']
 }));
+app.use(authRouter);
 
-app.get('/', (req, res) => {
-    res.send(`
-        <div>
-            Your id is: ${req.session.userId}
-            <form method="POST">
-                <input name="email" placeholder="email" />
-                <input name="password" placeholder="password" />
-                <input name="passwordConfirmation" placeholder="password confirmation" />
-                <button>Sign UP</button>
-            </form>
-        </div>
-    `);
-});
-
-
-app.post('/', async (req, res) => {
-    const { email, password, passwordConfirmation} = req.body;
-
-    const existingUser = await usersRepo.getOneBy({ email });
-
-    if (existingUser) {
-        return res.send('Email in use');
-    }
-
-    if (password !== passwordConfirmation) {
-        return res.send('Password must match');
-    }
-
-    // created
-    const user = await usersRepo.create({ email, password });
-
-    // store the id inside cookie
-    req.session.userId = user.id;
-
-    res.send(`
-        <div>Account created!</div>
-    `);
-});
 
 app.listen(3000, () => {
     console.log('Listening');
---
+++ routes/admin/auth.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const { check, validationResult } = require('express-validator');
+const usersRepo = require('../../repositories/users');
+const signupTemplate = require('../../views/admin/auth/signup');
+const signinTemplate = require('../../views/admin/auth/signin');
+
+const router = express.Router();
+
+router.get('/signup', (req, res) => {
+    res.send(signupTemplate({req}));
+});
+
+router.post('/signup', [
+    check('email')
+        .trim()
+        .normalizeEmail()
+        .isEmail(), 
+    check('password')
+        .trim()
+        .isLength({min: 4, max: 20 }), 
+    check('passwordConfirmation')
+        .trim()
+        .isLength({min: 4, max: 20 }) 
+    ], async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    
+    const { email, password, passwordConfirmation } = req.body;
+
+    const existingUser = await usersRepo.getOneBy({ email });
+
+    if (existingUser) {
+        return res.send('Email in use');
+    }
+
+    if (password !== passwordConfirmation) {
+        return res.send('Password must match');
+    }
+
+    // created
+    const user = await usersRepo.create({ email, password });
+
+    // store the id inside cookie
+    req.session.userId = user.id;
+
+    res.send(`
+        <div>Account created!</div>
+    `);
+});
+
+router.get('/signout', async (req, res) => {
+    req.session = null;
+    res.send("You are logged out!");
+});
+
+router.get('/signin', async (req, res) => {
+    res.send(signinTemplate());
+});
+
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    const user = await usersRepo.getOneBy({ email });
+    if (!user) {
+        return res.send('Email not found!');
+    }
+
+    const validPassword = await usersRepo.comparePasswords(user.password, password);
+    if (!validPassword) {
+        return res.send('Invalid password');
+    }
+
+    req.session.userId = user.id;
+    res.send('You are signed in!');
+});
+
+module.exports = router;
