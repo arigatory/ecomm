40072937fefaba2ccaaf3d42ecfe22c383ed0d83
Ivan MSI <arigatory@gmail.com>
2022-04-21 04:48:58 +0300

    add new prod and better styling

--- routes/admin/validators.js
+++ routes/admin/validators.js
@@ -2,6 +2,15 @@ const { check } = require('express-validator');
 const usersRepo = require('../../repositories/users');
 
 module.exports = {
+    requireTitle: check('title')
+        .trim()
+        .isLength({ min: 5, max: 40 })
+        .withMessage('Must be between 5 and 40 characters'),
+    requirePrice: check('price')
+        .trim()
+        .toFloat()
+        .isFloat({ min: 1 })
+        .withMessage('Must be a number between greater than 1'),
     requireEmail: check('email')
         .trim()
         .normalizeEmail()
@@ -27,27 +36,27 @@ module.exports = {
             }
         }),
     requireEmailExists: check('email')
-    .trim()
-    .normalizeEmail()
-    .isEmail()
-    .withMessage('Must provide a valid email')
-    .custom(async (email) => {
-        const user = await usersRepo.getOneBy({email});
-        if (!user) {
-            throw new Error('Email not found!');
-        }
-    }),
+        .trim()
+        .normalizeEmail()
+        .isEmail()
+        .withMessage('Must provide a valid email')
+        .custom(async (email) => {
+            const user = await usersRepo.getOneBy({ email });
+            if (!user) {
+                throw new Error('Email not found!');
+            }
+        }),
     requireValidPasswordForUser: check('password')
-		.trim()
-		.custom(async (password, { req }) => {
-			const user = await usersRepo.getOneBy({ email: req.body.email });
-			if (!user) {
-				throw new Error('Password not found!');
-			}
-			
-			const validPassword = await usersRepo.comparePasswords(user.password, password);
-			if (!validPassword) {
-				throw new Error('Invalid password');
-			}
-		})
+        .trim()
+        .custom(async (password, { req }) => {
+            const user = await usersRepo.getOneBy({ email: req.body.email });
+            if (!user) {
+                throw new Error('Password not found!');
+            }
+
+            const validPassword = await usersRepo.comparePasswords(user.password, password);
+            if (!validPassword) {
+                throw new Error('Invalid password');
+            }
+        })
 };
